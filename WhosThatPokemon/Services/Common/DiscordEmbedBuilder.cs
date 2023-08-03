2d3f1caf8fdf6f9ce8fc15463f197a559e7db234
akatoch <akatoch@gmail.com>
2023-08-03 21:48:21 +0530

    Changed formating for pokemon name to h3 tag

--- WhosThatPokemon/Services/Common/DiscordEmbedBuilder.cs
+++ WhosThatPokemon/Services/Common/DiscordEmbedBuilder.cs
@@ -32,7 +32,7 @@ namespace WhosThatPokemon.Services.Common
         public static Task BuildPokemonPredictionModel(EmbedBuilder embedBuilder, Pokemon pokemon)
         {
             string pokemonName = TextUtil.ChangeToPascalCase(pokemon.PokemonName);
-            embedBuilder.AddField("Pokemon Name: ", "`" + pokemonName + "`");
+            embedBuilder.AddField("Pokemon Name: ", "```<h3>" + pokemonName + "</h3>```");
             return Task.CompletedTask;
         }
 
